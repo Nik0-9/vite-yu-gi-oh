6a4ff6469d4bb4a150b05398d64714f28d8f8334
Nik0-9 <filannino.nicola9@gmail.com>
2024-04-15 18:50:01 +0200

    ADD LoaderComponent

--- src/App.vue
+++ src/App.vue
@@ -4,39 +4,41 @@
 </template>
 
 <script>
-import {store} from './store.js';
+import { store } from './store.js';
 import axios from 'axios';
 import HeaderComponent from './components/HeaderComponent.vue';
 import MainComponent from './components/mainComponents/MainComponent.vue'
 
-  export default {
-    name: 'App',
-    components:{
-      HeaderComponent,
-      MainComponent
-    },
-    data(){
-      return{
-        store
-      }
-    },
-    methods:{
-      getCard(){
-        axios.get(this.store.apiUrl).then((res)=>{
-          this.store.cards = res.data.data;
-          this.store.numCards = res.data.meta.current_rows;
-          console.log(res.data.data);
-          console.log(this.store.cards);
-
-        });
-      }
-    },
-    created(){
-      this.getCard();
+export default {
+  name: 'App',
+  components: {
+    HeaderComponent,
+    MainComponent
+  },
+  data() {
+    return {
+      store
     }
+  },
+  methods: {
+    getCard() {
+      this.store.loading = true;
+
+      axios.get(this.store.apiUrl).then((res) => {
+        this.store.cards = res.data.data;
+        this.store.numCards = res.data.meta.current_rows;
+        console.log(res.data.data);
+        console.log(this.store.cards);
+      }).finally(()=>{
+        // always executed
+        this.store.loading = false;
+      });
+    }
+  },
+  created() {
+    this.getCard();
   }
+}
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
--- src/components/mainComponents/CardsList.vue
+++ src/components/mainComponents/CardsList.vue
@@ -1,12 +1,13 @@
 <template>
     <div class="p-4">
         <NumCards />
-        <div class="row row-cols-2 row-cols-lg-5 g-2 g-lg-3">
+        <div class="row row-cols-2 row-cols-lg-5 g-2 g-lg-3" v-if="!store.loading">
             <div class="col" v-for="card in store.cards">
                 <CardComponent :image="card.card_images[0].image_url" :id="card.id" :title="card.name"
                     :archetype="card.archetype" />
             </div>
         </div>
+        <ApiLoader v-else /> 
     </div>
 </template>
 
@@ -14,12 +15,14 @@
 import { store } from '../../store.js';
 import NumCards from './NumCards.vue';
 import CardComponent from './CardComponent.vue';
+import ApiLoader from './ApiLoader.vue'
 
 export default {
     name: 'CardsList',
     components: {
         NumCards,
-        CardComponent
+        CardComponent,
+        ApiLoader
     },
     data() {
         return {
